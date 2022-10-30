7503c917b2619a4771292bf6e6ff79cff012ebdd
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2022-10-30 16:44:25 +0000

    playcount list done

--- src/Week5/CW1.java
+++ src/Week5/CW1.java
@@ -7,16 +7,16 @@ public class CW1 {
      public static final String CONSOLE_CLEAR = "\033[H\033[2J";
     public static void main(String[] args)
     {
-        Music ariana = new Music( "Ariana Grande", "Into you     ", 955507809);
-        Music xxxtentacion = new Music( "XXXTENTACION", "SAD          ", 1186079489);
-        Music biggie = new Music( "Notorious BIG", "Juicy        ", 486309193);
-        Music kendrick = new Music( "Kendrick Lamar", "Money tree    ", 114593544);
-        Music pac = new Music( "2Pac           ", "Hit 'Em Up   ", 561790810);
-        Music eminem = new Music( "Eminem     ", "Mockingbird    ", 646154834);
-        Music drake = new Music( "Drake        ", "God's Plan    ", 1452369081);
-        Music travis = new Music( "Travis Scott", "SICKO MODE    ", 1048820838);
-        Music joji = new Music( "Joji         ", "YEAH RIGHT    ", 143051257);
-        Music lilpeep = new Music( "Lil Peep    ", "Star Shopping", 120851437);
+        Music ariana = new Music( "ariana grande", "Into you", 1);
+        Music xxxtentacion = new Music( "xxxtentacion", "SAD", 9);
+        Music biggie = new Music( "notorious big", "Juicy", 8);
+        Music kendrick = new Music( "kendrick lamar", "Money tree", 7);
+        Music pac = new Music( "2pac", "Hit 'Em Up", 6);
+        Music eminem = new Music( "eminem", "Mockingbird", 5);
+        Music drake = new Music( "drake", "God's Plan", 4);
+        Music travis = new Music( "travis scott", "SICKO MODE", 1);
+        Music joji = new Music( "joji", "YEAH RIGHT", 2);
+        Music lilpeep = new Music( "lil peep", "Star Shopping", 3);
         
         ArrayList<Music> musics = new ArrayList<Music>();
         musics.add(ariana);
@@ -30,26 +30,56 @@ public class CW1 {
         musics.add(joji);
         musics.add(lilpeep);
         
-        for(Music music : musics)
-        {
-            music.print();
-        }
+        //PrintList(musics);
+        System.out.println(CONSOLE_CLEAR);
+
+        System.out.println("Songs listed by playcount : ");
+        PrintListByCount(musics);
+
+
         String remove = InputReader.getString("please insert the artist's first name if you want to remove a song : ");
         remove.toLowerCase();
         for (Music music : musics)
         {
-            if(music.getmusic() == musics)
-            return musics;
+            
+            if(music.getname().toLowerCase() == remove)
+            {
+                System.out.println(remove +" " + music.getname()); 
+                musics.remove(music);
+                
+            }
+            else 
+            System.out.println("song hasn't found. "); 
         }
-        return null; 
+        PrintList(musics);
 
+       } 
+       public static void  PrintList(ArrayList<Music> musics){
+        for(Music music : musics)
+        {
+            music.print();
+        }
+       }
+       public static void PrintListByCount(ArrayList<Music> localMusics){
+        
+        int howLong = localMusics.size();
+            for(int highest=0 ; highest< howLong; highest++ )
+            {
+                Music temp = new Music("", "", 0);
+                int highestNumber=0;
+                for (Music music : localMusics)
+                {
+                    if( highestNumber < music.getPlayCount())
+                    {
+                        highestNumber = music.getPlayCount();
+                        temp = music;
+
+                    }
+                }
+                temp.print();
+                localMusics.remove(temp);
+            }
        }
 
-        
-
-
-        
-    
-
     }
-}
+
